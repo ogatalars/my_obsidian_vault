25404f4d2b2130798fa83acf58dcdbb92654237a
ogatalars <ogataogara@gmail.com>
2025-09-26 15:30:52 -0300

    vault backup: 2025-09-26 15:30:52

---
+++ Studies/go/variables.go
@@ -0,0 +1,22 @@
+package main 
+
+import "fmt"
+
+func main() {
+
+    var a = "initial"
+    fmt.Println(a)
+
+    var b, c int = 1,2 
+    fmt.Println(b, c)
+
+    var d = true
+    fmt.Println(d)
+
+    var e int
+    fmt.Println(e)
+
+    f := "maça" // := syntax is shorthand for delcaring and initializing a variable.
+
+
+}
