1e2c3bf5184c8b0c14fa07406f034c89ca3e0f28
ogatalars <ogataogara@gmail.com>
2025-09-27 18:43:16 -0300

    vault backup: 2025-09-27 18:43:16

---
+++ Studies/go/constants.go
@@ -0,0 +1,20 @@
+package main 
+
+import (
+    "fmt"
+    "math"
+)
+
+const s string = "constant"
+
+func main() {
+    fmt.Println(s)
+
+    const n = 5000000
+    const d = 3wa2 / n
+
+    fmt.Println(d)
+    fmt.Println(int64(d))
+    fmt.Println(math.Sin(n))
+    
+}
