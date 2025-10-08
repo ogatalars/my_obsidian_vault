d454cd1beb568455bfdbf8290b4a39fefdb032c9
ogatalars <ogataogara@gmail.com>
2025-10-08 17:09:46 -0300

    vault backup: 2025-10-08 17:09:45

---
+++ Studies/JavaScript/bullet-forEach.js
@@ -0,0 +1,16 @@
+// É um metodo, função built-in do JS. USADO EM ARRAY
+
+const doces = ['bala jujuba', 'bala azeda', 'babaloo', 'donuts', 'sonho', 'chocolate']
+
+doces.forEach(function(item){console.log('-' + item)})
+
+// outra maneira
+doces.forEach(doceEspecifico => console.log(doceEspecifico))
+
+//o valor retornado do .forEach é sempre não definido
+
+//exemplo abaixo 
+
+const frutas = ['manga', 'coco', 'banana', 'abacaxi', 'uva']
+
+frutas.forEach(frutas => console.log(`Eu quero comer ${frutas}`))
