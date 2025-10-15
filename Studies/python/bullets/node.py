03d379e9f528b429fa409a780436e56ff1f8b5ee
ogatalars <ogataogara@gmail.com>
2025-10-15 19:49:07 -0300

    vault backup: 2025-10-15 19:49:07

---
+++ Studies/python/bullets/node.py
@@ -0,0 +1,19 @@
+# linked_lists
+
+class Node:
+    def __init__(value, next_node=None):
+      self.value = value
+      self.next_node = node
+
+    def get_value(self): 
+        return self.value
+
+    def get_next_node(self):
+        return self.next_node  
+
+
+    def set_next_node(self, next_node): 
+
+        self.next_node = next_node
+
+my_node = Node(44)                
