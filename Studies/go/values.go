27f6c04ca39f2d1c883f719ec4a5fddde5fa97dc
ogatalars <ogataogara@gmail.com>
2025-10-17 15:27:14 -0300

    vault backup: 2025-10-17 15:27:14

--- Studies/go/values.go
+++ Studies/go/values.go
@@ -1,13 +1,14 @@
 package main 
 
 import 'fmt'
-
+// operações
 func main() {
 
-    fmt.Println("go" + "lang") // string
+    fmt.Println("go" + "lang") // string - concatenação
 
-    fmt.Println("1+1 = ", 1+1) //numeros
-    fmt.Println("7.0/3.0 =", 7.0/3.0) // numeros inteiros e float
+    fmt.Println("1+1 = ", 1+1) //numeros - 
+    fmt.Println("7.0/3.0 =", 7.0/3.0) // numeros inteiros e float -> divisao
+    fmt.Println(1*5) // multiplicação
 
     fmt.Println(true && false) // boolean -> condição e
     fmt.Println(true || false) // condição ou
