10e997e58b5aae44d81741175024f973a0b25242
ogatalars <ogataogara@gmail.com>
2025-10-19 12:53:45 -0300

    vault backup: 2025-10-19 12:53:45

--- Studies/JavaScript/bullets/exercises_basic.js
+++ Studies/JavaScript/bullets/exercises_basic.js
@@ -1,10 +1,22 @@
-// Reversed array -> Write a function, reverseArray(), that takes in an array as an argument and returns a new array with the elements in the reverse order.
+// 1 - Reversed array -> Write a function, reverseArray(), that takes in an array as an argument and returns a new array with the elements in the reverse order.
 
-There’s a built-in array method called .reverse() that does a lot of this work for you, but you’re NOT allowed to use it here!
+//There’s a built-in array method called .reverse() that does a lot of this work for you, but you’re NOT allowed to use it here!
 const reverseArray = (array) => {
     let reverse = []
     for(let i = array.length -1; i >= 0; i--) {
         reverse.push(array[i])
     }
     return reverse
+}
+
+/* greetAliens() Write a function, greetAliens(), that takes in an array of strings and uses a for loop to print a greeting with each string in the array.
+
+The greeting should take the following format:
+“Oh powerful [stringElement], we humans offer our unconditional surrender!”
+*/ 
+
+const greetAliens = (arr) => {
+    for(let i = 0; i <= arr.length; i++) {
+        console.log(`Oh powerful ${arr[i]}, we humans offer our unconditional surrender!`)
+    }
 }
