b9c72af7ddce03aca19e9e4174053d4e2f33fb9f
ogatalars <ogataogara@gmail.com>
2025-10-21 19:58:42 -0300

    vault backup: 2025-10-21 19:58:42

--- Studies/JavaScript/bullets/BasicReview.js
+++ Studies/JavaScript/bullets/BasicReview.js
@@ -124,3 +124,60 @@ const exibirUsuario = async () => {
 }
 
 exibirUsuario()
+
+/*
+Sincrono (travado) => devemos esperar o retorno da função para a programação continuar a acontecer
+Assincrono -> não devemos esperar, quando estiver pronto ele chega.
+Promise -> ideia de promessa -> é um objeto que representa essa espera. É um container para um valor que ainda NÃO conhecemos. 
+pending -> pendente -> estado inicial 
+fulfiled -> realizada sucesso -> a operação foi concluiida
+rejected -> a operação falhou 
+fetch => É a ferramenta morderna do navegador para fazer requisições de rede (pedidos a um servidor/API). Quando usamos fetch() ele retorna IMEDIATAMENTE UMA PROMISE
+async -> coloca antes de declarar uma função, avisa ao js que é operação assync
+await -> dentro da função com async, na frente de qualquer coisa que retorne uma promise. 
+
+*/
+
+async function buscarUsuarioGitHub() {
+    try{
+        console.log("Iniciando uma busca...")
+        const response = await fetch('https://api.github.com/users/google')
+        const dados = await response.json();
+        console.log("Busca concluída!");
+        console.log("Nome do usuário:", dados.name);
+        console.log("Biografia:", dados.bio);
+    } catch(error) {
+        console.error("erro: ", error);
+    }
+}
+
+buscarUsuarioGitHub();
+
+// Closure
+function criarContador() {
+  // 1. Esta variável 'contagem' vive dentro de 'criarContador'
+  let contagem = 0;
+
+  // 2. Esta função interna é criada.
+  // Ela "enxerga" a variável 'contagem'
+  const funcaoInterna = function() {
+    contagem++; // Ela modifica a variável da "mochila"
+    console.log(contagem);
+  };
+
+  // 3. A função pai retorna a função interna (e sua "mochila")
+  return funcaoInterna;
+}
+
+// 4. Chamamos a função pai. Ela executa e morre.
+// 'meuContador' agora é a 'funcaoInterna' com a "mochila"
+const meuContador = criarContador();
+
+// 5. Cada vez que chamamos 'meuContador',
+// ele acessa e modifica a 'contagem' em sua mochila.
+meuContador(); // Imprime: 1
+meuContador(); // Imprime: 2
+meuContador(); // Imprime: 3
+
+// A variável 'contagem' está protegida. Eu não consigo acessá-la
+// daqui de fora. console.log(contagem) daria erro.
---
+++ Studies/Typescript/basicReview.ts
@@ -0,0 +1,7 @@
+// Typescript é um superset do JS, isso significa que ele adiciona tipos estáticos. 
+// a principal diferença é que ele adiciona tipagem, que seria : tipo depois de uma variavel ou parâmetro de função.
+
+
+let nome: string = "Sabrina"; 
+let idade: number = 28; 
+let possuiHobbies: boolean = true; 
