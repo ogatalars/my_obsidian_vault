c62dd01c647d88e22f1ad05794887ddcae43a34f
ogatalars <ogataogara@gmail.com>
2025-10-22 16:19:28 -0300

    vault backup: 2025-10-22 16:19:28

--- Studies/JavaScript/bullets/exercises_basic.js
+++ Studies/JavaScript/bullets/exercises_basic.js
@@ -21,3 +21,12 @@ const greetAliens = (arr) => {
     }
 }
 
+/* 
+Write a function sortYears() that takes in an array of years, and, using the built-in .sort() method, returns that array with the years sorted in descending order.
+
+You can test your function when you’re ready by passing in the years array or by making your own array of years!
+*/ 
+
+const sortYears = (arr) => {
+    return arr.sort((a,b) => b - a)
+}
