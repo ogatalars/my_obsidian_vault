bd124163b6a9edcbb4cd4ecd0fcd3df20986639b
ogatalars <ogataogara@gmail.com>
2025-10-23 18:20:35 -0300

    vault backup: 2025-10-23 18:20:35

---
+++ Studies/go/map.go
@@ -0,0 +1,24 @@
+// maps são tipos de dados associativos padrões no GO (HASHES OU DICIONARIOS)
+
+// Para criar um map vazio, use o padrão make: make(map[tipo-chave]tipo-val)
+
+package main 
+
+import "fmt"
+
+func main () {
+    m := make(map[string]int)
+
+    m["k1"] = 7
+    m["k2"] = 13
+
+    fmt.Println("map: ", m)
+
+    v1 := m["k1"]
+    fmt.Println("v1: ", v1)
+
+    n := map[string]int{"foo": 1, "bar": 2}
+
+=
+
+}
