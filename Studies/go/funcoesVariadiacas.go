34d8906abec12637a86798d89246dad168f4e6cf
ogatalars <ogataogara@gmail.com>
2025-10-24 16:30:11 -0300

    vault backup: 2025-10-24 16:30:11

---
+++ Studies/go/funcoesVariadiacas.go
@@ -0,0 +1,12 @@
+package main 
+
+import "fmt"
+
+func soma(nums ...int) {
+    fmt.Print(nums, " ")
+    total := 0
+    for _, num := range nums {
+        total += num
+    }
+    fmt.Println(total)
+}
--- Studies/go/functions.go
+++ Studies/go/functions.go
@@ -10,6 +10,13 @@ func mais(a int, b int) int {
 func main() {
     res := mais(1, 5)
     fmt.Println("1 + 2 = ", res)
+    // Go exige retornos explícitos, ou seja, ele não vai retornar automaticamente o valor da última expressão
+    a, b := vals()
+    fmt.Println(a)
+    fmt.Println(b)
+
+    _, c := vals() // Se você quer apenas um subconjunto de valores de retorno, use o identificador em branco _.
+    fmt.Println(c)
 }
 
-// Go exige retornos explícitos, ou seja, ele não vai retornar automaticamente o valor da última expressão
+
