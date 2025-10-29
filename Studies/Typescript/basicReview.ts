2f507372b89423c740c5c8a02a39a94257da81eb
ogatalars <ogataogara@gmail.com>
2025-10-28 22:31:19 -0300

    vault backup: 2025-10-28 22:31:19

--- Studies/Typescript/basicReview.ts
+++ Studies/Typescript/basicReview.ts
@@ -10,3 +10,18 @@ let possuiHobbies: boolean = true;
 // unknown: o any mais seguro, usamos quando não sabemos o que será dado como tipo 
 // void: representa funções que não retornam nada; 
 // never: never acontece; 
+
+let nomeProduto = 'Laptop';
+let preco = 4500; 
+let disponivel: boolean;
+
+
+let coringa: any = 4; 
+coringa = "agora sou uma string";
+coringa.toUpperCase(); // funciona mas é bom evitar esse tipo de tipagem any
+
+let misterio: unknown = 4
+// A forma correta de usar 'unknown' é verificando o tipo
+if (typeof misterio === 'string') {
+  console.log(misterio.toUpperCase());
+}
