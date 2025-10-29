235c3e422ca2b901fe99e80fb858c9f18d90eaa1
ogatalars <ogataogara@gmail.com>
2025-10-28 22:42:06 -0300

    vault backup: 2025-10-28 22:42:06

--- Studies/Typescript/basicReview.ts
+++ Studies/Typescript/basicReview.ts
@@ -25,3 +25,40 @@ let misterio: unknown = 4
 if (typeof misterio === 'string') {
   console.log(misterio.toUpperCase());
 }
+
+// void -> NAO RETORNA VALOR
+function logarNoConsole(mensagem: string): void {
+    console.log(mensagem)
+}
+function naoRetornaValor(numeroPrintado: number): void {
+    console.log(numeroPrintado)
+}
+
+// never -> nunca retorna, ela PARA o app, mas retorna um never
+
+function disparErro(erro:string): never {
+    throw new Error(erro);
+}
+
+// Arrays -> 
+// podemos declarar de duas maneiras -> tipo[] ou Array<tipo>
+let numeros: number[] = [1,2,3,4,5,6];
+numeros.push("6"); // ERRO: Argument of type 'string' is not assignable to parameter of type 'number'.
+let nomes: string[] = ["Ariette", "Ama", "Will", "e", "Jesus", "Nicole"]
+
+let notas: Array<number> = [10, 7.5, 9];
+let frameWorks: Array<string> = ["React", "Vue", "Angular"]
+
+// Objetos => ao inves de criar objetos soltos, a gente define CONTRATO ou MOLDE ssendo que -> 
+// interface: Um contrato que define a forma de um objeto. Padrão 
+// type: um apelido (alias) para qualquer tipo
+interface IUsuario{
+    id: number; 
+    nome: string; 
+    ativo: boolean; 
+    email?: string // ? opcional o email
+}
+
+function cadastrarUsuario(usuario: IUsuario): void {
+    console.log(`Cadastrando ${usuario.nome}  (ID: ${usuario.id})`)
+}
